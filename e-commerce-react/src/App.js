d7a2052cffe5359ed494581f1204d7054e1a86d7
BeaMozer <beatriz.mozer@hotmail.com>
2023-06-29 12:12:54 -0300

    [configuração]criei os compoenentes principais do projeto

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -1,5 +1,15 @@
+import Filters from "./Components/Filters";
+import Home from "./Components/ProductList/Home";
+import Cart from "./Components/ShoppingCart/Cart";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Filters />
+      <Home />
+      <Cart />
+    </>
+  );
 }
 
 export default App;
